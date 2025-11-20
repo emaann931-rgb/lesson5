519dbf4e968703150d0bbef3ed27b17eb6ac9887
Noor13456 <noorgrps@gmail.com>
2025-11-20 21:15:01 +0500

    4

--- activity1.py
+++ activity1.py
@@ -1,1 +1,20 @@
-sdg
+#x = "10"         
+y = int(x)        
+print(y, type(y))
+
+a = "3.14"        
+b = float(a)      
+print(b, type(b))
+
+
+num = 5           
+result = float(num)   
+print(result, type(result))
+
+num = 5           # integer value
+result = float(num)   # convert integer to f
+
+x = "False"  
+y = bool(x)        
+print(y)
+''''''
